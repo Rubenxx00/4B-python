8513c6ea106dddfa8c18b78e12f43b58cddd49a7
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-04-27 09:02:44 +0200

    modifiche bolletta

--- acqua/calcolatore_tariffe.py
+++ acqua/calcolatore_tariffe.py
@@ -2,7 +2,7 @@
 ALIQUOTA = 10
 LIMITE_1S = 80
 PREZZO_1S = 0.30
-PREZZO_2S = 0.30
+PREZZO_2S = 0.35
 QUOTA_PRIVATI = 10
 QUOTA_AZIENDE = 20
 
@@ -15,13 +15,13 @@ QUOTA_AZIENDE = 20
 def calcola_privati(consumo):
     totale = _calcolo_importo_base(consumo)
     totale += QUOTA_PRIVATI
-    totale = totale + (totale*ALIQUOTA)
+    totale = totale + (totale*ALIQUOTA/100)
     return totale
 
 def calcola_aziende(consumo):
     totale = _calcolo_importo_base(consumo)
     totale += QUOTA_AZIENDE
-    totale = totale + (totale*ALIQUOTA)
+    totale = totale + (totale*ALIQUOTA/100)
     return totale
     
 # funzione ad uso interno del modulo!
@@ -31,4 +31,6 @@ def _calcolo_importo_base(consumo):
         totale = (LIMITE_1S * PREZZO_1S) + ((consumo - LIMITE_1S) * PREZZO_2S)
     else:
         totale = consumo * PREZZO_1S
+    # metodo alternativo
+    totale = min(LIMITE_1S, consumo) * PREZZO_1S + max((consumo - LIMITE_1S), 0) * PREZZO_2S
     return totale
--- acqua/programma.py
+++ acqua/programma.py
@@ -1,3 +1,4 @@
+# dal modulo importo solo le funzioni che mi interessano, nascondendo il resto
 from calcolatore_tariffe import calcola_aziende, calcola_privati
 
 def istogramma(li):
@@ -8,10 +9,11 @@ def istogramma(li):
 
 tipologia = input("privato o azienda? (p/a)")
 
-# TODO: chiedere consumo per ogni mese e calcolare l'importo corrispondente. Salvare i risultati e disegnare l'istogramma dei consumi nell'arco dei 12 mesi
 
-mesi = list(range(1,12+1))
+mesi = list(range(1,2+1))
 consumi = []
+tariffe = []
+totale_annuo = 0
 
 for m in mesi:
     consumo = int(input(f"inserisci consumo del mese {m}: "))
@@ -22,6 +24,15 @@ for m in mesi:
     else:
         tariffa = calcola_aziende(consumo)
 
+    totale_annuo += tariffa
+    tariffe.append(tariffa)
     consumi.append(consumo)
 
 istogramma(consumi)
+
+print("tariffe per mese: ", tariffe)
+print("consumi per mese: ", consumi)
+
+for m in mesi:
+    print(f'nel mese {m} hai consumato {consumi[m-1]} e speso {tariffe[m-1]:.2f} €')
+    #print("{0:.2f} €".format(tariffe[m-1]))
