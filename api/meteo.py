ae6cda9c020eec2da0cb008ee90c519ed8222e44
Rubenxx00 <ruben.scopacasa@gmail.com>
2022-05-27 09:11:36 +0200

    add air quality endopoint

--- api/meteo.py
+++ api/meteo.py
@@ -4,11 +4,39 @@ import requests
 
 key = "" # incolla la API key
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat=45.4654219&lon=9.1859243&appid={key}&lang=it&units=metric"
+lat, long = 0.0, 0.0
+while True:
+    try:
+        city = input("inserisci una città ")
+        geocode = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={key}"
+        risposta = requests.get(geocode)
+        risultati: list = risposta.json()
+        for i, el in enumerate(risultati):
+            print(f'{i+1}) {el["name"]}, {el["country"]}')
 
-risposta = requests.get(url)
+        index = int(input("Scegli uno dei risultati (immetti numero) ")) - 1
+        res = risultati[index]
+
+        lat = res["lat"]
+        lon = res["lon"]
+    except Exception:
+        print("e")
+    else:
+        break
+
+
+weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}&lang=it&units=metric"
+
+risposta = requests.get(weather_url)
 diz = risposta.json()
-print("meteo ->", diz["weather"])
+print("città ->", diz["name"])
+print("meteo ->", diz["weather"][0])
 print("vento ->", diz["wind"])
+print("temperature ->", diz["main"])
+
+air_quality_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={key}"
+risposta = requests.get(air_quality_url)
+diz = risposta.json()
+
 
 # programma che, dopo aver chiesto in input coordinate geografiche, chiami API e stampi il meteo
